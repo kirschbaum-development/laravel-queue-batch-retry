b717b5bedba03a287a8f924355e151ad814fbf73
Luís Dalmolin <luis.nh@gmail.com>
2022-08-17 15:34:06 -0300

    Pulling failed jobs table from config

--- src/FailedJob.php
+++ src/FailedJob.php
@@ -12,6 +12,15 @@ class FailedJob extends Model
 
     protected $casts = ['failed_at' => 'datetime'];
 
+    public function __construct(array $attributes = [])
+    {
+        if (config('queue.failed.table')) {
+            $this->table = config('queue.failed.table');
+        }
+
+        parent::__construct($attributes);
+    }
+
     public function setPayloadAttribute($payload)
     {
         $this->attributes['payload'] = serialize($payload);
